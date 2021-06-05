4fbae9fcff4f31ee784252b69bb2e099d8616beb
revolution-99 <60106442+revolution-99@users.noreply.github.com>
2021-06-05 23:14:58 +0530

    Update Noteinput.jsx

--- src/components/Noteinput.jsx
+++ src/components/Noteinput.jsx
@@ -1,62 +1,28 @@
-import React, { useState } from 'react';
-import { useStyles } from './styles';
+import React,{useState} from 'react';
 import Paper from '@material-ui/core/Paper';
-import NoteDisplay from './NoteDisplay';
-import uniqueString from 'unique-string';
+import { useStyles } from './styles1'
+import { colors } from '@material-ui/core';
 
-export default function Noteinput() {
-    const [note, setNote] = useState({id: " ", title: " " , content: " " });
-    const [notes, setNotes] = useState([]);
+
+export default function NoteDisplay({note, deleteNotes}) {
     const classes = useStyles();
+    const [color, setColor] = useState("fdfdfd");
 
-    const deleteNotes = (e) => {  
-        const newnotes = notes.filter((value) => {
-            return value.id !== e.target.value
-        })
-        setNotes(newnotes);
+    const setStyle = (color) =>{
+        setColor(color);
     }
-
-    // const editNotes = () => {  to do by satwik
-
-    // }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        setNotes((prevNotes) => {
-            return [...prevNotes, note]
-        });
-
-        console.log(notes);
-    }
-
     return (
-        
         <>
             <Paper elevation={3} className={classes.root}>
-                <form onSubmit={handleSubmit} style={{width: '90%'}}>
-                <div class="mb-3">
-                    <label className="form-label">Title</label>
-                    <input type="text" className="form-control" value={note.title} onChange={(e) => setNote({...note, title: e.target.value})}/>
+                <h4>{note.title}</h4>
+                <p>{note.content}</p>
+                <style>{color}</style>
+                <div className="car-button" style={{display: "flex"}}>
+                    <button class="btn btn-primary" type="submit" onClick={deleteNotes} value={note.id}>Delete</button>
+                    <span/>
+                    <button class="btn btn-primary" type="submit" onClick={()=>setStyle("#12B0E8")}>Bookmark</button>
                 </div>
-                <div className="mb-3">
-                    <label className="form-label">Write note</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={note.content} onChange={(e) => setNote({...note, content : e.target.value})}></textarea>
-                </div>
-                <button class="btn btn-primary" type="submit" onClick={() => setNote({...note, id: uniqueString()})}>Add Note</button>
-                </form>
             </Paper>
-            <div className="container" style={{position: 'relative', top: 100}}>
-                <div className="row">
-                    {notes.length === 0 ? null : 
-                    notes.map((note) => (
-                        <div className="col-sm-3">
-                            <NoteDisplay note={note} deleteNotes={deleteNotes}/>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            
         </>
     )
 }
