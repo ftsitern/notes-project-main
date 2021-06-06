4ff0a23d1a828ea0c2b6cbacbc321f87cf6d1cb6
Satwikan <satwikanmol25@gmail.com>
2021-06-06 09:36:25 +0530

    added editNotein NoteDisplay

--- src/components/NoteDisplay.jsx
+++ src/components/NoteDisplay.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import Paper from "@material-ui/core/Paper";
 import { useStyles } from "./styles1";
 
-export default function NoteDisplay({ note, deleteNotes }) {
+export default function NoteDisplay({ note, deleteNotes, editNotes }) {
   const classes = useStyles();
-  const [color, setColor] = useState("fdfdfd");
-
   return (
     <>
       <Paper elevation={3} className={classes.root}>
         <h4>{note.title}</h4>
         <p>{note.content}</p>
-        <div className="car-button" style={{ display: "flex" }}>
+        <div
+          className="car-button"
+          style={{ display: "flex", "justify-content": "space-around" }}
+        >
           <button
             class="btn btn-primary"
             type="submit"
@@ -19,13 +20,6 @@ export default function NoteDisplay({ note, deleteNotes }) {
             value={note.id}
           >
             Delete
-          </button>
-          <button
-            class="btn btn-primary"
-            type="submit"
-            onClick={() => setStyle("#12B0E8")}
-          >
-            Bookmark
           </button>
           <button
             class="btn btn-primary"
