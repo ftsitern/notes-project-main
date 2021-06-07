41a01b30d9189b2d328ba91e4bfa83c57a4635af
swayam-coder <swayam14feb@gmail.com>
2021-06-07 23:48:24 +0530

    final commit

--- src/components/NoteDisplay.jsx
+++ src/components/NoteDisplay.jsx
@@ -1,36 +1,60 @@
 import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import { useStyles } from "./styles1";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 import moment from "moment";
+import "./animation.css";
 
 export default function NoteDisplay({ note, deleteNotes, editNotes }) {
-  const classes = useStyles();
   const [isClicked, setClicked] = useState(false);
 
   const changeStyle = () => {
-      if(isClicked)
-          setClicked(false);
-      else
-          setClicked(true);
-  }
+    if (isClicked) setClicked(false);
+    else setClicked(true);
+  };
 
   return (
     <>
-      <Paper elevation={3} className={classes.root} style={{backgroundColor: "black"}}>
-          <h4>{note.title}</h4>
-        <p>{note.content}</p>
+      <Paper
+        elevation={3}
+        style={{
+          backgroundColor: "black",
+          "min-height": "7vh",
+          margin: "10px",
+          padding: 10,
+        }}
+        className="bounce-in-top"
+      >
+      <div style={{position: "relative", bottom: 20}}>
+            <div style={{
+                textAlign: 'right',
+                position: 'relative',
+                top: 20,
+                color: 'white'
+            }}>
+            <small>
+                {note.date}
+            </small>
+            </div>
+            <div style={{ "margin-left": "10px" }}>
+                <h4>{note.title}</h4>
+                <p>{note.content}</p>
+            </div>
+      </div>
         <div
           className="car-button"
-          style={{ display: "flex", "justify-content": "space-between" }}
+          style={{
+            display: "flex",
+            "justify-content": "space-around",
+            "align-items": "space-around",
+          }}
         >
           <button
             class="btn btn-primary"
             type="submit"
             onClick={deleteNotes}
             value={note.id}
-            style={{backgroundColor: "#ff8303"}}
+            style={{ backgroundColor: "#ff8303" }}
           >
             Delete
           </button>
@@ -39,13 +63,20 @@ export default function NoteDisplay({ note, deleteNotes, editNotes }) {
             type="submit"
             onClick={editNotes}
             value={note.id}
-            style={{backgroundColor: "#ff8303"}}
+            style={{ backgroundColor: "#ff8303" }}
           >
             Edit ✏
           </button>
-          <a type="button" onClick={changeStyle} style={{position: "relative", top: 5}}>{
-              isClicked ? <FavoriteIcon style={{color: "#ff8303"}}/> : <FavoriteBorderIcon style={{color: "#ff8303"}}/>
-          }
+          <a
+            type="button"
+            onClick={changeStyle}
+            style={{ position: "relative", top: 5 }}
+          >
+            {isClicked ? (
+              <FavoriteIcon style={{ color: "#ff8303" }} />
+            ) : (
+              <FavoriteBorderIcon style={{ color: "#ff8303" }} />
+            )}
           </a>
         </div>
       </Paper>
